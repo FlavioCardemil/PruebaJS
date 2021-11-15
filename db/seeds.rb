32ced3c0697cfc6d95ee30d85024631f3e2213ea
Flavio <flavio.cardemil@gmail.com>
2021-11-15 10:39:50 -0300

    Seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,7 @@
 
 # Bookmark.destroy_all
 
-# 20.times do
-#   Category.create(
-#     title: Faker::TvShows::SouthPark.character
-#   )
-# end
+
 
 # 20.times do
 #   Bookmark.create(
@@ -22,3 +18,54 @@
 #     type_id: Random.rand(1..10)  
 #   )
 # end
+
+
+require 'faker'
+
+
+20.times do
+  Type.create(
+    title: Faker::TvShows::SouthPark.character
+  )
+end
+
+categories = ["Autos", "Aviones", "Submarino"]
+
+categories.each do |categorie|
+    Category.create(
+        title: categorie
+    )
+end
+
+subcategories_1 = ["Mercedes", "Audi"]
+subcategories_2 = ["Monoplaza", "Biplano", "Comercial"]
+subcategories_3 = ["Nuclear", "Ligero"]
+
+subcategories_1 .each do |categorie|
+    Category.create(
+        title: categorie,
+        category_id: 1  
+    )
+end
+
+subcategories_2 .each do |categorie|
+    Category.create(
+        title: categorie,
+        category_id: 2
+    )
+end
+
+subcategories_3 .each do |categorie|
+    Category.create(
+        title: categorie,
+        category_id: 3
+    )
+end
+
+20.times do
+    Bookmark.create(
+        url: Faker::TvShows::HeyArnold.character,
+        category_id: Random.rand(1..5),
+        type_id: Random.rand(1..2)
+    )
+end
